28ad99d169d6dbd4b662cbb9e5dbfabdca9cc362
kylaerod <kylarodriguez52@gmail.com>
2024-04-10 22:12:36 -0700

    added path

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Set up your routes, middleware, and other configurations here
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3001; // Set the port with environment variable or default to 3001
 
