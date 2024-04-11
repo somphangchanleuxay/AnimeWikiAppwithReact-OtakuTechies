ec5d5a4057e93070e4ae8e4992e65b565ff97f83
David <d.robles1297@Yahoo.com>
2024-04-10 20:54:07 -0500

    server and connection.js update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Set up your routes, middleware, and other configurations here
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const port = process.env.PORT || 3001; // Set the port with environment variable or default to 3001
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
