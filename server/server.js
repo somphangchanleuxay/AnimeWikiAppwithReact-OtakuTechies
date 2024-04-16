fe4a310476a7c6caa6643a9f39826c33d6c0f2de
David <d.robles1297@Yahoo.com>
2024-04-15 21:45:03 -0500

    try #2

--- server/server.js
+++ server/server.js
@@ -1,25 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-// const path = require('path');
-
-// // Set up your routes, middleware, and other configurations here
-// app.use(express.static(path.join(__dirname, '../client/public')));
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
@@ -50,16 +28,13 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
-  // Serve static files in production
-  if (process.env.NODE_ENV === 'production') {
-    // Serve static files from the 'client/dist' directory
-    app.use(express.static(path.join(__dirname, '../client/public')));
+  // Serve static files from the 'client/build' directory
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-    // Serve index.html for all other routes
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/public/index.html'));
-    });
-  }
+  // Serve index.html for all other routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   // Once database connection is open, start listening for requests
   db.once('open', () => {
