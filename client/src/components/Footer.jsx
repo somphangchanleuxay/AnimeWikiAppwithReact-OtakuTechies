53fb8827a57f0ab61ef8fe34047b05a06eb762a8
kylaerod <kylarodriguez52@gmail.com>
2024-04-15 19:36:02 -0700

    removed bottom padding

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Flex, Link, Text } from '@chakra-ui/react';
 
 function Footer() {
   return (
-    <Box as="footer" bg="gray.800" color="white" py={4} px={8}>
+    <Box as="footer" bg="gray.800" color="white" py={4} px={8} bottom="0" left="0" width="100%">
       <Flex justify="center">
         <Text fontSize="md">
           <Link href="/about">About Us</Link>
@@ -22,4 +22,3 @@ function Footer() {
 }
 
 export default Footer;
-
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -34,7 +34,7 @@ const Homepage = () => {
         <p style={{ marginTop: '20px', fontFamily: 'Arial, sans-serif', fontSize: '24px', color: 'yellow' }}>
           You can look up your favorite anime/manga and sign in to blog.
         </p>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px', marginBottom: '500px' }}>
           <input type="text" placeholder="Search..." style={{ padding: '10px', fontSize: '16px', width: '300px', borderRadius: '5px', border: 'none', marginBottom: '10px' }} />
           <button style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: 'blue', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Search</button>
         </div>
