dce9755f48c2b6537a93f468b81b8c777bc22324
David <d.robles1297@Yahoo.com>
2024-04-15 21:04:40 -0500

    connection.js fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/otaku');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://datauser:superSecure@cluster0.athnlz7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
