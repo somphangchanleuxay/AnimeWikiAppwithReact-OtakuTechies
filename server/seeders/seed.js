f29f27be7b0f619dcf7e62a76860bcab3a5fd22e
M <melissabrown@Melissas-MacBook-Pro.local>
2024-04-17 21:57:55 -0500

    model anime

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,19 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Anime } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const cleanDB = require('./cleanDB');
+const animeSeeds = require('./animeSeeds');
 
 db.once('open', async () => {
   try {
     await cleanDB('User', 'users');
     await User.create(userSeeds);
+    await Anime.create(animeSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 
-  console.log('Users seeded successfully!');
+  console.log('Users and anime seeded successfully!');
   process.exit(0);
 });
