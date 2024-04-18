65a60e3f808274ae1d1c41c4d76d7457f7c52dac
David <d.robles1297@Yahoo.com>
2024-04-17 19:23:05 -0500

    trying to import _uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,10 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-// Use a meaningful default connection string if MONGODB_URI is not defined
-const defaultURI = 'mongodb://localhost:27017/otaku_db';
-
-mongoose.connect(process.env.MONGODB_URI || defaultURI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // Add other options if needed
 });
 
-export default mongoose.connection;
+module.exports = mongoose.connection;
