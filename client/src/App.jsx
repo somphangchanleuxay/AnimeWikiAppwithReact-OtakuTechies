ba3a375547cc1c09de9173b12354f3284612cbc5
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2024-04-17 19:58:46 -0700

    updated apollo client code inside app.jsx and tested login, login successful

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,10 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { ChakraProvider, Center } from '@chakra-ui/react'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -7,8 +14,30 @@ import Contact from './components/Contact';
 import LoginSignup from './components/LoginSignup'; 
 import './index.css';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
+    <ApolloProvider client={client}>
     <ChakraProvider>
       <Router>
         <Center>
@@ -22,6 +51,7 @@ const App = () => {
         </Routes>
       </Router>
     </ChakraProvider>
+    </ApolloProvider>
   );
 };
 
