99edd9c47b2c98e51d1e2a9b2917475e0f135e72
David <d.robles1297@Yahoo.com>
2024-04-17 19:38:37 -0500

    Notes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Environment variables was defined in Render.com dashboard
+// MONGODB_URI was defined in Render 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
