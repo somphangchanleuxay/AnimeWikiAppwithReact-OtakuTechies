5ff44bd9d0c656a6c0fd244b4575165de1d25bcf
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2024-04-19 20:43:47 -0700

    added css rule to adjust margin to center out spacing of the footer text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,17 @@
 import { Box, Flex, Link, Text } from '@chakra-ui/react';
+import '../css/Footer.css'; 
 
 function Footer() {
   return (
     <Box as="footer" bg="gray.800" color="white" py={4} px={8} bottom="0" left="0" width="100%">
-      <Flex justify="center">
-        <Text fontSize="md">
+      <Flex justify="center" align="center">
+        <Text fontSize="md" className="footer-link">
           <Link href="/about">About Us</Link>
         </Text>
-        <Text fontSize="md" mx={4}>
+        <Text fontSize="md" className="footer-link">
           <Link href="/contact">Contact Us</Link>
         </Text>
-        <Text fontSize="md" mx={4}>
+        <Text fontSize="md" className="footer-link">
           <Link href="/services">Our Services</Link>
         </Text>
       </Flex>
