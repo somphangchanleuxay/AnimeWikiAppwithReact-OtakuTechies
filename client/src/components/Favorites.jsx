f03ba33cf0250bfa094eba37f7ff41449b4d0bb4
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2024-04-24 00:22:21 -0700

    updated Favorites to render Users favorites in the favorites.jsx, updated typedefs, mutations. User is able to remove/add favorited anime to complete updated delete from graphql

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,18 +1,13 @@
-
-import { gql, useQuery } from '@apollo/client';
-import { FaHeart } from 'react-icons/fa';
-import Footer from './Footer'; 
+import { gql, useQuery } from "@apollo/client";
+import Footer from "./Footer";
 
 const GET_FAVORITES = gql`
   query GetFavorites {
     me {
       _id
-      favorites {
-        _id
-        title
-        description
-        image
-      }
+      username
+      email
+      favorites
     }
   }
 `;
@@ -24,11 +19,13 @@ const Favorites = () => {
   if (error) return <p>Error :(</p>;
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-    }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <div
         style={{
           display: "flex",
@@ -43,17 +40,31 @@ const Favorites = () => {
           padding: "20px",
         }}
       >
-
         <div>
-          <h1>Favorite Anime</h1>
-          {data.me.favorites.map((anime) => (
-            <div key={anime._id} className="favoriteAnimeContainer">
-              <div style={{ marginBottom: '10px', width: '300px', height: '300px' }}>
-                <img src={anime.image} alt={anime.title} style={{ borderRadius: '5px', width: '100%', height: '100%' }} />
-              </div>
-              <h2 style={{ fontWeight: 'bold', fontSize: '24px' }}>{anime.title}</h2>
-              <p>{anime.description}</p>
-              <FaHeart style={{ color: 'red' }} />
+          <h1
+            style={{
+              fontWeight: "bold",
+              fontSize: "50px",
+              color: "white",
+              fontFamily: "Unica One, Arial",
+              textDecoration: "underline",
+            }}
+          >
+            Favorited Animes
+          </h1>
+
+          {data.me.favorites.map((anime, index) => (
+            <div key={index} className="favoriteAnimeContainer">
+              <h2
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "24px",
+                  color: "red",
+                  fontFamily: "Unica One; Arial",
+                }}
+              >
+                {anime}
+              </h2>
             </div>
           ))}
         </div>
