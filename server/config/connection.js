ebd33f959f1e02e32f09ad4adf95604c7287f32d
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2024-04-24 11:26:07 -0700

    had to update proxy for front end code to work with backend to right location

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Environment variables was defined in Render.com dashboard
 // MONGODB_URI was defined in Render 
 mongoose.connect(
- process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017',
+ process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
